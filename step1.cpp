e8d4f95710745d09bd377887eb95ec402b332023
Simon Oyas <sdoyas@cpp.edu>
2022-05-20 11:55:25 -0700

    Part 1

---
+++ step1.cpp
@@ -0,0 +1,67 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+int ledPin = 2;
+
+// WiFi
+const char *ssid = "Oyas"; // Enter your WiFi name
+const char *password = "7144713266";  // Enter WiFi password
+
+// MQTT Broker
+const char *mqtt_broker = "192.168.0.32";
+const char *topic = "esp32/test";
+const int mqtt_port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+ // LED Output
+ pinMode(ledPin, OUTPUT);
+ // Set software serial baud to 115200;
+ Serial.begin(115200);
+ // connecting to a WiFi network
+ WiFi.begin(ssid, password);
+ while (WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.println("Connecting to WiFi..");
+ }
+ Serial.println("Connected to the WiFi network");
+ //connecting to a mqtt broker
+ client.setServer(mqtt_broker, mqtt_port);
+ client.setCallback(callback);
+ while (!client.connected()) {
+     String client_id = "esp32-client-";
+     client_id += String(WiFi.macAddress());
+     Serial.printf("The client %s connects to the mqtt broker\n", client_id.c_str());
+     if (client.connect(client_id.c_str())){
+         Serial.println("MQTT broker connected");
+     } else {
+         Serial.print("failed with state ");
+         Serial.print(client.state());
+         delay(2000);
+     }
+ }
+ // publish and subscribe
+ client.publish(topic, "Hi I'm ESP32 ^^");
+ client.subscribe(topic);
+}
+
+void callback(char *topic, byte *payload, unsigned int length) {
+ Serial.print("Message arrived in topic: ");
+ Serial.println(topic);
+ Serial.print("Message:");
+ String message;
+ for (int i = 0; i < length; i++) {
+     message = message + (char) payload[i];
+ }
+  Serial.print(message);
+  if (message == "1") { digitalWrite(led1Pin, HIGH); } // LED on
+  if (message == "2") { digitalWrite(led1Pin, LOW); } // LED off 
+  Serial.println();
+  Serial.println("-----------------------");
+}
+
+void loop() {
+ client.loop();
+}
